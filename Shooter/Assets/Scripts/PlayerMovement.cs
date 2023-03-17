bdd0a5c20e9d0ba4f029fa38d99d8bdb8bf82b59
Andy Tran <54690164+aktran321@users.noreply.github.com>
2023-03-17 05:07:08 -0400

    Zombie running animation
    created animation for zombie running. Also created transitions with the conditions of when to switch from idle to running and back to idle. Specifically, created the parameter "isRunning" withing Unity which is a boolean paramter.

--- Shooter/Assets/Scripts/PlayerMovement.cs
+++ Shooter/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,11 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 12f;
     Vector3 velocity;
     public float gravity = -9.81f;
+    public bool isGrounded;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    public float jumpHeight = 2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +22,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
         float x = Input.GetAxis("Horizontal");
 
         float z = Input.GetAxis("Vertical");
@@ -28,5 +39,10 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(move * speed * Time.deltaTime);
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
     }
 }
